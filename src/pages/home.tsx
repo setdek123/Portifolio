54c9c957fabb419ce7ae52d2593671b72089ba60
setdek123 <rodriguezalan785@gmail.com>
2026-03-03 08:03:25 -0300

    Fazendo a parte da responsividade homePege

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,13 +9,13 @@ const Home = () => {
         <div className="flex h-screen items-center justify-around bg-[#0E0E0E] px-23 py-35">
             <div className="flex flex-col w-220 ">
                 <div className="flex flex-col gap-5">
-                    <h2 className="text-white font-bold text-4xl">Olá!</h2>
-                    <h1 className="font-semibold text-white text-7xl">IM <strong className="font-extrabold text-[#7456FF]">Alan rodrigues</strong></h1>
-                    <h3 className="text-[#999999] text-2xl">Ui Designer Junior</h3>
+                    <h2 className="text-white font-bold md:text-4xl -mt-10 text-2xl">Olá!</h2>
+                    <h1 className="font-semibold text-white md:text-7xl text-4xl">IM <strong className="font-extrabold text-[#7456FF]">Alan rodrigues</strong></h1>
+                    <h3 className="text-[#999999] md:text-2xl text-xl">Ui Designer Junior</h3>
                 </div>
                 <div className="flex flex-col gap-10">
                     <span className="text-gray-300 font-bold">___________</span>
-                    <span className="text-[#999999] text-3xl">
+                    <span className="text-[#999999] md:text-3xl text-2xl">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto 
                         praesentium id voluptas 
                         laboriosam quis. Itaque a nihil officiis repellat perspiciatis 
@@ -29,25 +29,29 @@ const Home = () => {
                 </div>
             </div>
             <div className="flex flex-col items-center gap-28">
-                <div className=''>
-                    <div className='border-[#7456FF] border border-[#7456FF] w-110 h-110 items-center flex justify-center rounded-full  bg-[#161324]'>
-                        <img src={profile} className='w-100 rounded-full ' />
-                    </div>
-                    
+                
+                <div className='border-[#7456FF] border md:w-110 top-20 right-25 md:relative absolute md:h-110 h-25 w-25 items-center flex justify-center rounded-full  bg-[#161324]'>
+                    <img src={profile} className='md:w-100 w-20 rounded-full ' />
                 </div>
-                <div className="flex gap-10 items-center mt-20 ">
-                    <p className="text-white font-semibold">Find Me On</p>
-                    <div className='bg-[#161324] p-3 rounded-full'>
-                        <MdWhatsapp size={25} color='#7456FF'/>
+                    
+                
+                <div className="flex gap-10 items-center md:flex md:items-center md:justify-center -ml-17  md:-ml-70 md:mt-15 bottom-0 left-30 md:relative absolute ">
+                    <div className='flex flex-col mt-20'>
+                        <p className="text-white font-semibold hidden md:flex">Find Me On</p>
                     </div>
-                    <div className='bg-[#161324] p-3 rounded-full'>
-                        <MdFacebook size={25} color='#7456FF'/>
-                    </div>
-                    <div className='bg-[#161324] p-3 rounded-full'>
-                        <FaInstagram size={25} color='#7456FF'/>
-                    </div>
-                    <div className='bg-[#161324] p-3 rounded-full'>
-                        <FaLinkedin size={25} color='#7456FF'/>
+                    <div className='flex gap-10 items-center mt-20'>
+                        <div className='bg-[#161324] p-3 rounded-full'>
+                            <MdWhatsapp size={25} color='#7456FF'/>
+                        </div>
+                        <div className='bg-[#161324] p-3 rounded-full'>
+                            <MdFacebook size={25} color='#7456FF'/>
+                        </div>
+                        <div className='bg-[#161324] p-3 rounded-full'>
+                            <FaInstagram size={25} color='#7456FF'/>
+                        </div>
+                        <div className='bg-[#161324] p-3 rounded-full'>
+                            <FaLinkedin size={25} color='#7456FF'/>
+                        </div>
                     </div>
                     
                 </div>
